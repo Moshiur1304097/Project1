2ec7c28e20865e58ebcc5becfc2a89fe8fc89e8d
shihabbu056 <shihabbu056@yahoo.com>
2019-07-02 20:18:56 +0600

    NewAdded

--- MyProject/MyProject/Form1.Designer.cs
+++ MyProject/MyProject/Form1.Designer.cs
@@ -28,10 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button saveButton;
+            saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            saveButton.Location = new System.Drawing.Point(329, 183);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new System.Drawing.Size(75, 23);
+            saveButton.TabIndex = 0;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(saveButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MyProject/MyProject/Form1.cs
+++ MyProject/MyProject/Form1.cs
@@ -16,5 +16,8 @@ namespace MyProject
         {
             InitializeComponent();
         }
+
+       
+        
     }
 }
